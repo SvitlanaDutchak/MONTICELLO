c3fede78d256d9e5b670e17fe72a8a6d67f4b64d
“SvitlanaDutchak” <kaneki.ken123777@gmail.com>
2022-06-20 17:47:40 +0300

    Added project(without Responsive)

--- js/script.js
+++ js/script.js
@@ -1,24 +1,225 @@
+$(document).ready(function () {
 
   $('.slider').slick({
     dots: true,
     infinite: false,
     speed: 300,
     slidesToShow: 3,
-    slidesToScroll: 4,
-    responsive: [
-      {
+    slidesToScroll: 1,
+    responsive: [{
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
         }
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
         }
       }
     ]
   });
+  
+  $('.hero-slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true,
+    autoplay: true,
+  })
+  
+  $('.menu li a').click(function () { // 1
+		elementClick = $(this).attr('href');
+		destination = $(elementClick).offset().top;
+		$('body,html').animate({scrollTop: destination }, 1400);
+	});
+
+});
+
+const mapStyles = [{
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#f5f5f5"
+    }]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#616161"
+    }]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [{
+      "color": "#f5f5f5"
+    }]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "geometry.stroke",
+    "stylers": [{
+      "color": "#ac6c6c"
+    }]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#bdbdbd"
+    }]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#eeeeee"
+    }]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#757575"
+    }]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#e5e5e5"
+    }]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry.fill",
+    "stylers": [{
+      "color": "#dedede"
+    }]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#9e9e9e"
+    }]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#ffffff"
+    }]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#757575"
+    }]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#dadada"
+    }]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#616161"
+    }]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#9e9e9e"
+    }]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#e5e5e5"
+    }]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#eeeeee"
+    }]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#c9c9c9"
+    }]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry.fill",
+    "stylers": [{
+      "color": "#c7c7c7"
+    }]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [{
+      "color": "#9e9e9e"
+    }]
+  }
+];
+
+const footer = document.querySelector('.footer');
+const btnDown = document.querySelectorAll('.hero__btn-down');
+
+for (let i = 0; i < btnDown.length; i++) {
+  btnDown[i].addEventListener("click", function (event) {
+    footer.scrollIntoView({
+        behavior: "smooth"
+      }),
+
+      event.preventDefault()
+
+  })
+}
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: 40.682122301957975,
+      lng: -74.0209732429698
+    },
+    zoom: 12,
+    styles: mapStyles,
+  });
+
+  const marker = "img/icons/circle-solid (1).svg";
+
+  new google.maps.Marker({
+    position: {
+      lat: 40.687872473718095,
+      lng: -73.90846439331666
+    },
+    map,
+    title: "OUR LOCATION",
+    icon: marker
+  });
+}
+
+window.initMap = initMap();
